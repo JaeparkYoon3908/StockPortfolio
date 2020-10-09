1f20a4d60b47e47483a9ebdd8d4acb0115a2ed2d
Kimgumho <54883589+Kimgumho@users.noreply.github.com>
2020-10-09 22:13:48 +0900

    체크박스 추가

--- app/src/main/java/com/yjpapp/stockportfolio/activity/MainActivity.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yjpapp.stockportfolio.activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yjpapp.stockportfolio.R
 import com.yjpapp.stockportfolio.adapter.MainListAdapter
@@ -14,7 +15,10 @@ import com.yjpapp.stockportfolio.util.Utils
 import kotlin.collections.ArrayList
 
 
-class MainActivity : RootActivity(R.layout.activity_main) {
+class MainActivity : RootActivity(R.layout.activity_main), MainListAdapter.OnDeleteMode {
+
+    private var isDeleteMode: Boolean = false
+    private var mainListAdapter: MainListAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         logcat(Utils.getTodayYYYYMMDD())
@@ -30,29 +34,49 @@ class MainActivity : RootActivity(R.layout.activity_main) {
             arrayList.add(dataInfo)
         }
 
-        val mainListAdapter = MainListAdapter(arrayList)
+        mainListAdapter = MainListAdapter(arrayList, this)
         recyclerview_MainActivity.adapter = mainListAdapter
         //img_back.setColorFilter(getColor(R.color.white))
 
         //리스너 등록
         //lin_add.setOnClickListener(onClickListener)
-
+        lin_bottom_menu_left.setOnClickListener(onClickListener)
+        lin_bottom_menu_right.setOnClickListener(onClickListener)
     }
 
     private val onClickListener = View.OnClickListener {view: View? ->
         when(view?.id){
-            /*
-            R.id.lin_add -> {
-                val intent = Intent(mContext, AddActivity::class.java)
-                startActivity(intent)
+            R.id.lin_bottom_menu_left -> {
+                //delete 모드에서 왼쪽 버튼을 누름
+                if(isDeleteMode){
+                    Toast.makeText(this@MainActivity, "deleteMode On!!!", Toast.LENGTH_SHORT).show()
+                    txt_bottom_menu_left.text = getString(R.string.common_modify)
+                    txt_bottom_menu_right.text = getString(R.string.common_add)
+                    mainListAdapter?.hideCheckBox()
+                    mainListAdapter?.notifyDataSetChanged()
+                    isDeleteMode = false
+                }else{
+                    Toast.makeText(this@MainActivity, "deleteMode Off!!!", Toast.LENGTH_SHORT).show()
+                }
             }
-             */
+            R.id.lin_bottom_menu_right -> {
+                //delete 모드에서 오른쪽 버튼을 누름
+                if(isDeleteMode){
+                    Toast.makeText(this@MainActivity, "deleteMode On!!!", Toast.LENGTH_SHORT).show()
+                    txt_bottom_menu_left.text = getString(R.string.common_modify)
+                    txt_bottom_menu_right.text = getString(R.string.common_add)
+                    mainListAdapter?.hideCheckBox()
+                    mainListAdapter?.notifyDataSetChanged()
+                    isDeleteMode = false
+                }else{
 
+                }
+            }
         }
     }
 
     // DB 생성 코드
-    private fun createDB(){
+    private fun createDB() {
         val dbHelper = DatabaseOpenHelper(this)
         val sqliteDatabases = dbHelper.writableDatabase
         val databaseHandler = DatabaseHandler(sqliteDatabases, dbHelper)
@@ -61,5 +85,13 @@ class MainActivity : RootActivity(R.layout.activity_main) {
         dataInfo.gainPercent = "10%"
         dataInfo.realPainLossesAmount = "test"
         databaseHandler.insertData(dataInfo)
+    }
+
+    override fun deleteModeOn() {
+        isDeleteMode = true
+        txt_bottom_menu_left.text = getString(R.string.common_cancel)
+        txt_bottom_menu_right.text = getString(R.string.common_complete)
+
+        logcat("deleteModeOn 콜백 왔어!!")
     }
 }
--- app/src/main/java/com/yjpapp/stockportfolio/adapter/MainListAdapter.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/adapter/MainListAdapter.kt
@@ -4,17 +4,26 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.recyclerview.widget.RecyclerView
 import com.yjpapp.stockportfolio.R
 import com.yjpapp.stockportfolio.model.DataInfo
+import kotlinx.android.synthetic.main.item_main_list.view.*
 
-class MainListAdapter(private val data: ArrayList<DataInfo>) :
+class MainListAdapter(private val data: ArrayList<DataInfo>, callback: OnDeleteMode) :
     RecyclerView.Adapter<MainListAdapter.ViewHolder>(){
     private var mContext: Context? = null
     private var arrayList = ArrayList<DataInfo>()
+    private var onDeleteMode: OnDeleteMode
+    private var hideCheckBox: Boolean = true
+
+    interface OnDeleteMode{
+        fun deleteModeOn()
+    }
 
     init {
         this.arrayList = data
+        this.onDeleteMode = callback
     }
 
     class ViewHolder(var view: View) : RecyclerView.ViewHolder(view)
@@ -27,12 +36,25 @@ class MainListAdapter(private val data: ArrayList<DataInfo>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // 아이템을 길게 눌렀을 때 삭제모드로 전환.
         holder.itemView.setOnLongClickListener {
+            hideCheckBox = false
+            onDeleteMode.deleteModeOn()
+            notifyDataSetChanged()
             return@setOnLongClickListener true
+        }
+        if(hideCheckBox){
+            holder.itemView.checkbox.visibility = View.GONE
+        }else{
+            holder.itemView.checkbox.visibility = View.VISIBLE
         }
     }
 
     override fun getItemCount(): Int {
         return arrayList.size
+    }
+
+    fun hideCheckBox(){
+        hideCheckBox = true
     }
 }
