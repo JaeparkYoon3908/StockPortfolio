e67457fb6f70fb91007d6330c5e622c9e8605c8d
kimgumho <54883589+kimgumho@users.noreply.github.com>
2020-12-14 14:26:55 +0900

    bottom filter 애니메이션 추가. (Dialog => BottomSheetFragment 변경)

--- app/src/main/java/com/yjpapp/stockportfolio/ui/dialog/MainFilterDialog.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/ui/dialog/MainFilterDialog.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.yjpapp.stockportfolio.R
+import com.yjpapp.stockportfolio.util.Utils
 import kotlinx.android.synthetic.main.dailog_main_filter.*
 
-class MainFilterDialog(callback: MainFilterDialog.MainFilterClicked): BottomSheetDialogFragment() {
+class MainFilterDialog(callback: MainFilterClicked): BottomSheetDialogFragment() {
     interface MainFilterClicked{
         fun allSelect()
         fun gainSelect()
@@ -24,6 +26,11 @@ class MainFilterDialog(callback: MainFilterDialog.MainFilterClicked): BottomShee
         super.onAttach(context)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setStyle(STYLE_NORMAL, R.style.AppBottomSheetDialogTheme)
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         rootView = inflater.inflate(R.layout.dailog_main_filter, container, false)
@@ -33,6 +40,13 @@ class MainFilterDialog(callback: MainFilterDialog.MainFilterClicked): BottomShee
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         initLayout()
         super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val bottomSheet: View? = dialog?.findViewById(R.id.design_bottom_sheet)
+        BottomSheetBehavior.from(bottomSheet!!).peekHeight = Utils.dpToPx(190)
     }
 
     private val onClickListener = View.OnClickListener { view: View? ->
