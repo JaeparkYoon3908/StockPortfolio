379e16f8f8a800c18ee20e39ce26309844fc4e9b
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-06-26 22:12:12 +0900

    path 추가

--- server_dev/wsgi.py
+++ server_dev/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/ubuntu/docker-server/ReviewServer/')
+
+sys.path.append('/home/ubuntu/docker-server/ReviewServer/venv/Scripts/')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server_dev.settings')
 
+from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
+
