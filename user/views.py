fdc01fe6b0c148dcd0c0f2e7f5e5b265323f8c20
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-07-07 21:55:37 +0900

    이미 존재하는 이메일 에러 처리

--- user/views.py
+++ user/views.py
@@ -8,10 +8,18 @@ from .models import UserInfo
 
 class RegistUser(APIView):
     def post(self, request):
-        user_email = request.data.get('user_email', "") # 클라이언트에서 올리는 user_id
-        user_name = request.data.get('user_name', "") # 클라이언트에서 올리는 user_pw
+        user_email = request.data.get('user_email', "")  # 클라이언트에서 올리는 user_id
+        user_name = request.data.get('user_name', "")  # 클라이언트에서 올리는 user_pw
 
-        UserInfo.objects.create(user_email=user_email, user_name=user_name) # LoginUser 모델에 새로운 object 생성
+        if UserInfo.objects.filter(user_email=user_email).exists():
+            user = UserInfo.objects.filter(user_email=user_email).first()
+            data = dict(
+                status=600,
+                msg="이미 존재하는 이메일입니다."
+            )
+            return Response(data)
+
+        UserInfo.objects.create(user_email=user_email, user_name=user_name)  # LoginUser 모델에 새로운 object 생성
 
         # 클라이언트한테 내려줄 데이터 정의
         data = dict(
