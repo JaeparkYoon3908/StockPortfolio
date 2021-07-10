1025034cb729c56a7286344be328bf1166f090fb
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-07-10 12:15:20 +0900

    userindex 추가

--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -14,13 +14,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='LoginUser',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('user_id', models.CharField(default=False, max_length=20)),
                 ('user_pw', models.CharField(default=False, max_length=20)),
             ],
             options={
                 'verbose_name': '로그인 테스트 테이블',
-                'db_table': 'login_user',
+                'db_table': 'user_info',
             },
         ),
     ]
--- user/models.py
+++ user/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 
 class UserInfo(models.Model):
+    user_index = models.IntegerField(primary_key=True, null=False, db_index=True, auto_created=True)
     user_email = models.CharField(max_length=20, null=False, default=False)
-    user_name = models.CharField(max_length=20, null=False, default=False)
+    user_name = models.CharField(max_length=20, null=True, default=False)
+    # user_token = models.CharField()
 
     class Meta:
         db_table = 'user_info'
