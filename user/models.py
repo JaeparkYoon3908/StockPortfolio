991e279b4fb73a4967ceff30e847f5df6c52d7aa
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-07-19 20:56:27 +0900

    로그인 타입, 유저 인덱스 추가

--- user/models.py
+++ user/models.py
@@ -5,9 +5,10 @@ from django.db import models
 
 
 class UserInfo(models.Model):
-    user_index = models.IntegerField(primary_key=True, null=False, auto_created=True)
+    user_index = models.AutoField(primary_key=True, null=False)
     user_email = models.CharField(max_length=20, null=False, default=False)
     user_name = models.CharField(max_length=20, null=False, default=False)
+    login_type = models.CharField(max_length=20, null=True, default=False)
 
     class Meta:
         db_table = 'user_info'
--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 # user/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+import user.models
 from .models import UserInfo
 
 
@@ -8,13 +10,18 @@ class RegistUser(APIView):
     def post(self, request):
         user_email = request.data.get('user_email', "") # 클라이언트에서 올리는 user_id
         user_name = request.data.get('user_name', "") # 클라이언트에서 올리는 user_pw
+        login_type = request.data.get('login_type', "") # 클라이언트에서 올리는 login_type
 
-        UserInfo.objects.create(user_index=user_index, user_email=user_email, user_name=user_name) # LoginUser 모델에 새로운 object 생성
+        UserInfo.objects.create(user_email=user_email, user_name=user_name, login_type=login_type) # LoginUser 모델에 새로운 object 생성
 
         # 클라이언트한테 내려줄 데이터 정의
         data = dict(
-            user_email=user_email,
-            user_name=user_name
+            status=200,
+            data=dict(
+                user_email=user_email,
+                user_name=user_name,
+                login_type=login_type
+            )
         )
 
         return Response(data=data)
