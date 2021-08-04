53752f2a3b94aab205ac437258033252eb67812d
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-08-04 17:54:52 +0900

    SwipeLayout 모듈화 성공

---
+++ libraries/swipeLayout/src/main/java/com/yjpapp/swipelayout/interfaces/SwipeAdapterInterface.java
@@ -0,0 +1,9 @@
+package com.yjpapp.swipelayout.interfaces;
+
+public interface SwipeAdapterInterface {
+
+    int getSwipeLayoutResourceId(int position);
+
+    void notifyDatasetChanged();
+
+}
---
+++ libraries/swipeLayout/src/main/java/com/yjpapp/swipelayout/interfaces/SwipeItemMangerInterface.java
@@ -0,0 +1,29 @@
+package com.yjpapp.swipelayout.interfaces;
+
+import com.yjpapp.swipelayout.SwipeLayout;
+import com.yjpapp.swipelayout.util.Attributes;
+
+import java.util.List;
+
+public interface SwipeItemMangerInterface {
+
+    void openItem(int position);
+
+    void closeItem(int position);
+
+    void closeAllExcept(SwipeLayout layout);
+    
+    void closeAllItems();
+
+    List<Integer> getOpenItems();
+
+    List<SwipeLayout> getOpenLayouts();
+
+    void removeShownLayouts(SwipeLayout layout);
+
+    boolean isOpen(int position);
+
+    Attributes.Mode getMode();
+
+    void setMode(Attributes.Mode mode);
+}
