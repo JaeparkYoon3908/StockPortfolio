2dc0f74bed1c057d1a0342466cd2598ac94a03c2
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-09-02 22:42:25 +0900

    test token url 삭제

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,4 @@ from . import views
 urlpatterns = [
     url('regist_user', views.RegistUser.as_view(), name='regist_user'),
     url('user_info', views.SelectUser.as_view(), name='user_info'),
-    url('test_token', views.TestTokenAuth.as_view(), name='test_token'),
 ]
