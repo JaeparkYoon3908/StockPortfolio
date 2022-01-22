ebf7d77af5ab76bf7151780786387236e1302a50
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2022-01-22 20:42:09 +0900

    1.4.7 버전 릴리즈, dependencies 버전 업데이트

--- app/src/main/java/com/yjpapp/stockportfolio/util/StockLog.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/util/StockLog.kt
@@ -5,18 +5,18 @@ import com.yjpapp.stockportfolio.BuildConfig
 
 object StockLog {
     fun d(tag: String?, msg: String) {
-//        if (BuildConfig.LOG_CAT) Log.d(tag, msg)
+        if (BuildConfig.LOG_CAT) Log.d(tag, msg)
     }
 
     fun e(tag: String?, msg: String) {
-//        if (BuildConfig.LOG_CAT) Log.e(tag, msg)
+        if (BuildConfig.LOG_CAT) Log.e(tag, msg)
     }
 
     fun i(tag: String?, msg: String) {
-//        if (BuildConfig.LOG_CAT) Log.i(tag, msg)
+        if (BuildConfig.LOG_CAT) Log.i(tag, msg)
     }
 
     fun v(tag: String?, msg: String) {
-//        if (BuildConfig.LOG_CAT) Log.v(tag, msg)
+        if (BuildConfig.LOG_CAT) Log.v(tag, msg)
     }
 }
