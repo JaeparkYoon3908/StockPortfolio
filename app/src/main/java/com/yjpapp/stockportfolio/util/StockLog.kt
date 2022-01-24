adc9ad648c17fb02d04a4fafe704aafcef6e962e
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2022-01-24 23:09:25 +0900

    Log 함수 인자 추가

--- app/src/main/java/com/yjpapp/stockportfolio/util/StockLog.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/util/StockLog.kt
@@ -8,6 +8,10 @@ object StockLog {
         if (BuildConfig.LOG_CAT) Log.d(tag, msg)
     }
 
+    fun d(tag: String?, msg: String, tr: Throwable) {
+        if (BuildConfig.LOG_CAT) Log.d(tag, msg, tr)
+    }
+
     fun e(tag: String?, msg: String) {
         if (BuildConfig.LOG_CAT) Log.e(tag, msg)
     }
@@ -20,7 +24,27 @@ object StockLog {
         if (BuildConfig.LOG_CAT) Log.i(tag, msg)
     }
 
+    fun i(tag: String?, msg: String, tr: Throwable) {
+        if (BuildConfig.LOG_CAT) Log.i(tag, msg, tr)
+    }
+
     fun v(tag: String?, msg: String) {
         if (BuildConfig.LOG_CAT) Log.v(tag, msg)
+    }
+
+    fun v(tag: String?, msg: String, tr: Throwable) {
+        if (BuildConfig.LOG_CAT) Log.v(tag, msg, tr)
+    }
+
+    fun wtf(tag: String?, msg: String) {
+        if (BuildConfig.LOG_CAT) Log.wtf(tag, msg)
+    }
+    
+    fun wtf(tag: String?, tr: Throwable) {
+        if (BuildConfig.LOG_CAT) Log.wtf(tag, tr)
+    }
+
+    fun wtf(tag: String?, msg: String, tr: Throwable) {
+        if (BuildConfig.LOG_CAT) Log.wtf(tag, msg, tr)
     }
 }
