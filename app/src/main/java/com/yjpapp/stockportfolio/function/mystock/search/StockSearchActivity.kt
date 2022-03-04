d2266998e691f4f75ff759ee1cba960590233776
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2022-03-04 16:30:34 +0900

    주식 검색 디자인 변경

--- app/src/main/java/com/yjpapp/stockportfolio/function/mystock/search/StockSearchActivity.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/function/mystock/search/StockSearchActivity.kt
@@ -1,23 +1,27 @@
 package com.yjpapp.stockportfolio.function.mystock.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldColors
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.compose.ui.unit.dp
 import com.yjpapp.stockportfolio.common.theme.Color_222222
+import com.yjpapp.stockportfolio.common.theme.Color_4876C7
+import com.yjpapp.stockportfolio.common.theme.Color_CD4632
+import com.yjpapp.stockportfolio.common.theme.Color_FFFFFF
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -34,23 +38,55 @@ class StockSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                Top()
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .background(Color_FFFFFF)
+            ){
+                SearchBar()
             }
         }
     }
     @Preview
     @Composable
-    private fun Top() {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+    private fun SearchBar() {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+                .background(Color_FFFFFF)
         ) {
-            Text(
-                text = "StockSearchFragment",
-                fontSize = 16.sp,
-                color = Color_222222
+            var searchKeyWord by remember { mutableStateOf("")}
+            TextField(
+                value = searchKeyWord,
+                onValueChange = {
+                    searchKeyWord = it
+                },
+                maxLines = 1,
+                singleLine = true,
+                trailingIcon = {
+                    Icon(Icons.Filled.Search, "", tint = Color_222222)
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color_FFFFFF,
+                    focusedIndicatorColor = Color_222222,
+                    cursorColor = Color_222222
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+
             )
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun SearchList() {
+        LazyColumn {
+
         }
     }
 }
