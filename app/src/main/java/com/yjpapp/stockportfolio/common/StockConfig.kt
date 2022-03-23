d2c00c25abf520c07b9a3dce9ec00e0dd2a7d9ed
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2022-03-23 16:28:44 +0900

    하단 배너 광고 추가

--- app/src/main/java/com/yjpapp/stockportfolio/common/StockConfig.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/common/StockConfig.kt
@@ -38,6 +38,6 @@ object StockConfig {
     /**
      * AdMob
      */
-    const val AD_MOB_ID = "pub-6147567013464925"
+    const val AD_MOB_ID = "ca-app-pub-6147567013464925~2757588331"
     const val TEST_AD_MOB_ID = "ca-app-pub-3940256099942544/6300978111"
 }
--- app/src/main/java/com/yjpapp/stockportfolio/function/MainActivity.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/function/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
         AdView(this).apply {
             adSize = AdSize.BANNER
-            adUnitId = StockConfig.TEST_AD_MOB_ID
+            adUnitId = StockConfig.AD_MOB_ID
         }
 
         MobileAds.initialize(this) {}
