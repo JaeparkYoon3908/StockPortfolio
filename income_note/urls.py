0a29025d0dfea5f569a59ebefc92bcefd88314c8
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2022-06-27 21:51:42 +0900

    테스트 api 추가

--- income_note/urls.py
+++ income_note/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url('list', views.IncomeNoteAPI.as_view(), name='income_note'),
-    url('total_gain', views.TotalGainAPI.as_view(), name='total_gain')
+    url('total_gain', views.TotalGainAPI.as_view(), name='total_gain'),
+    url('test_api', views.TestAPI.as_view(), name='test_api'),
 ]
