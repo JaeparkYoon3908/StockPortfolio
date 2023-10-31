a64837af588e3986d2c180f0a0975276e369ceb8
윤재박 <yoonjaepark1122@gmail.com>
2023-10-31 09:43:41 +0900

    모듈화 - data, network 분리 완료

--- app/src/main/java/com/yjpapp/stockportfolio/ui/main/mystock/dialog/MyStockPurchaseInputDialogContent.kt
+++ app/src/main/java/com/yjpapp/stockportfolio/ui/main/mystock/dialog/MyStockPurchaseInputDialogContent.kt
@@ -43,8 +43,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.yjpapp.network.model.StockPriceInfo
 import com.yjpapp.stockportfolio.R
-import com.yjpapp.data.model.response.StockPriceInfo
 import com.yjpapp.stockportfolio.extension.getSerializableExtraData
 import com.yjpapp.stockportfolio.ui.common.StockConfig
 import com.yjpapp.stockportfolio.ui.common.dialog.CommonDatePickerDialog
