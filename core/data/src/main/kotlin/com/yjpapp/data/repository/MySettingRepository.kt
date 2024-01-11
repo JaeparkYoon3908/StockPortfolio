0f7834860435ac31bb3bf73d2d93af61158d16c3
윤재박 <yoonjaepark1122@gmail.com>
2024-01-11 16:25:48 +0900

    MySettingRepository에서 ioDispacher삭제
    Retrofit2 라이브러리 분석 도중 알게된 것중에 enqueue 메소드는 결국 Excutor를 사용하여 백그라운드 스레드 풀에서 동작 하고 네트워킹이 끝난 후 Main Thread로 반환하는것을 알게 됐다. 즉, 디스크 캐싱이나 추가 무거운 작업이 아니면 굳이 ioDispacher를 사용하여 컨텍스트 체인지를 할 필요가 없다.

--- core/data/src/main/kotlin/com/yjpapp/data/repository/MySettingRepository.kt
+++ core/data/src/main/kotlin/com/yjpapp/data/repository/MySettingRepository.kt
@@ -18,47 +18,54 @@ class MySettingRepositoryImpl @Inject constructor(
 ) : MySettingRepository {
     private val defaultMyStockTitleKey = "DefaultMyStockTitle"
     private val isFirstAppRunKey = "isFirstAppRun"
-    private val ioDispatcher = Dispatchers.IO
     override suspend fun setDefaultMyStockTitle(title: String): ResponseResult<Boolean> {
-        return withContext(ioDispatcher) {
-            try {
-                pref.setPreference(defaultMyStockTitleKey, title)
-                ResponseResult.Success(data = true, resultCode = "200", resultMessage = "OK")
-            } catch (e: Exception) {
-                ResponseResult.Error(data = false, errorCode = "500", errorMessage = e.message?: "Unknown error")
-            }
+        return try {
+            pref.setPreference(defaultMyStockTitleKey, title)
+            ResponseResult.Success(data = true, resultCode = "200", resultMessage = "OK")
+        } catch (e: Exception) {
+            ResponseResult.Error(
+                data = false,
+                errorCode = "500",
+                errorMessage = e.message ?: "Unknown error"
+            )
         }
     }
 
     override suspend fun getDefaultMyStockTitle(): ResponseResult<String> {
-        return withContext(ioDispatcher) {
-            try {
-                val data = pref.getPreference(defaultMyStockTitleKey)
-                ResponseResult.Success(data = data, resultCode = "200", resultMessage = "OK")
-            } catch (e: Exception) {
-                ResponseResult.Error(errorCode = "500", errorMessage = e.message?: "Unknown error")
-            }
+        return try {
+            val data = pref.getPreference(defaultMyStockTitleKey)
+            ResponseResult.Success(data = data, resultCode = "200", resultMessage = "OK")
+        } catch (e: Exception) {
+            ResponseResult.Error(errorCode = "500", errorMessage = e.message ?: "Unknown error")
         }
     }
 
     override suspend fun setIsFirstAppRun(value: Boolean): ResponseResult<Boolean> {
-        return withContext(ioDispatcher) {
-            try {
-                pref.setPreference(isFirstAppRunKey, value)
-                ResponseResult.Success(data = true, resultCode = "200", resultMessage = "OK")
-            } catch (e: Exception) {
-                ResponseResult.Error(data = false, errorCode = "500", errorMessage = e.message?: "Unknown error")
-            }
+        return try {
+            pref.setPreference(isFirstAppRunKey, value)
+            ResponseResult.Success(data = true, resultCode = "200", resultMessage = "OK")
+        } catch (e: Exception) {
+            ResponseResult.Error(
+                data = false,
+                errorCode = "500",
+                errorMessage = e.message ?: "Unknown error"
+            )
         }
     }
 
     override suspend fun getIsFirstAppRun(): ResponseResult<Boolean> {
-        return withContext(ioDispatcher) {
-            try {
-                ResponseResult.Success(data = pref.getPreference(isFirstAppRunKey) == "true", resultCode = "200", resultMessage = "OK")
-            } catch (e: Exception) {
-                ResponseResult.Error(data = false, errorCode = "500", errorMessage = e.message?: "Unknown error")
-            }
+        return try {
+            ResponseResult.Success(
+                data = pref.getPreference(isFirstAppRunKey) == "true",
+                resultCode = "200",
+                resultMessage = "OK"
+            )
+        } catch (e: Exception) {
+            ResponseResult.Error(
+                data = false,
+                errorCode = "500",
+                errorMessage = e.message ?: "Unknown error"
+            )
         }
     }
 }
